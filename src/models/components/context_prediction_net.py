cef862e296a9dd038e5e22d449da441a19a8f7ab
Artem Makoian <amakoian@constructor.university>
2024-10-17 01:03:19 +0000

    jigsaw puzzles permutations pretrain

--- src/models/components/context_prediction_net.py
+++ src/models/components/context_prediction_net.py
@@ -5,12 +5,12 @@ import torch.nn as nn
 
 
 class ContextPredictionNet(nn.Module):
-    def __init__(self, num_classes: int, encoder: nn.Module, ignored_layers: List[str]):
+    def __init__(self, num_classes: int, encoder: nn.Module, ignored_layers: List[str], fc: List[nn.Module] = None):
         super().__init__()
         self.encoder = encoder
         for name in ignored_layers:
             setattr(self.encoder, name, nn.Identity())
-        self.fc = nn.LazyLinear(num_classes)
+        self.fc = nn.Sequential(*fc) or LazyLinear(num_classes)
 
     def forward(self, inputs: List[torch.Tensor]):
         outs = [self.encoder(inp) for inp in inputs]
